4a1240d84abd9b9311b35805e9ddfe4208b39ed4
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2022-02-19 20:36:48 -0600

    created xamarin tabbed project

---
+++ TheBoredButton/TheBoredButton/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TheBoredButton.ViewModels;
+using Xamarin.Forms;
+
+namespace TheBoredButton.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
